fd8d339e0417521f3e09e05e365a7e6159136e85
Pierre Dahrouj <pdahrouj7@gmail.com>
2024-10-15 11:39:44 -0700

    fizzbuzz challenge

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Program
+{
+
+
+    static void Main(string[] args)
+    {
+        FizzBuzz(100);
+    }
+
+    static void FizzBuzz(int num)
+    {
+        for (int i = 0; i < num; i++)
+        {
+            if (i % 5 == 0 && i % 3 == 0)
+            {
+                Console.WriteLine("FizzBuzz");
+            }
+            else if (i % 5 == 0)
+            {
+                Console.WriteLine("Buzz");
+            }
+            else if (i % 3 == 0)
+            {
+                Console.WriteLine("Fizz");
+            }
+            else
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+    }
+}
